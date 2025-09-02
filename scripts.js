515be3aa022dd27617daee27fd42a21c548448da
Julian <julilei276@gmail.com>
2025-09-01 23:04:26 -0300

    Agrego pag productos

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Pagina Productos Individuales
+
+const imgProductos = document.getElementById("imagenesProductos");
+const h1 = document.createElement("h1");
+
+h1.textContent = "Hola";
+
+imgProductos.appendChild(h1);
