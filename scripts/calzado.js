9dce5fc3aa8a042d092facc049a0d055eebe7d3e
Julian <julilei276@gmail.com>
2025-09-08 12:30:03 -0300

    Todas la paginas de categorias y link a nav

---
+++ scripts/calzado.js
@@ -0,0 +1,21 @@
+const contenedor = document.getElementById("productos")
+const nombresProductos = JSON.parse(localStorage.getItem("productosNombresGuardados")) || []
+productos.filter(prod => prod.categoria === "Calzado").forEach(prod =>{      //recorre todos los productos de la categoria
+    const div = document.createElement('div');
+    div.classList.add("carta");
+
+    //forma y contenido de la carta
+    div.innerHTML = `
+        <a href="productos.html?id=${prod.id}" class="nombre">${prod.nombre}</a>
+        <img src="" alt="Prod 1">
+        <p class="precio">${prod.precio}</p>
+        <div>
+            <span class="disminuir">-</span>
+            <span class="cantidad">1</span>
+            <span class="aumentar">+</span>
+        </div>
+        <p class="agregar">Agregar</p>
+    `;
+
+    contenedor.appendChild(div)
+})
